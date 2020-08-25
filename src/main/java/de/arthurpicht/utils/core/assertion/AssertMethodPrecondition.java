b265c9bc63e1d37ad4b913dd6a5d151f97af8ffa
arthurpicht <info@arthurpicht.de>
2020-08-25 09:34:49 +0200

    method names cleaned in AssertMethodPrecondition, deprecated old ones

--- src/main/java/de/arthurpicht/utils/core/assertion/AssertMethodPrecondition.java
+++ src/main/java/de/arthurpicht/utils/core/assertion/AssertMethodPrecondition.java
@@ -18,7 +18,7 @@ public class AssertMethodPrecondition {
         if (value == null) throw new MethodPreconditionFailedException("Parameter [" + name + "] is null.");
     }
 
-    public static void methodParamEndsWith(String name, String value, String endsWith) {
+    public static void parameterEndsWith(String name, String value, String endsWith) {
         parameterNotNull(name, "name");
         parameterNotNull(value, "value");
         parameterNotNull(endsWith, "endsWith");
@@ -26,17 +26,60 @@ public class AssertMethodPrecondition {
             throw new MethodPreconditionFailedException("Parameter [" + name + "] not ending with '" + endsWith + "'");
     }
 
-    public static void methodParamNotNullAndNotEmpty(String name, String value) {
+    /**
+     * @deprecated use {@link #parameterEndsWith(String, String, String)} instead
+     * @param name
+     * @param value
+     * @param endsWith
+     */
+    @Deprecated
+    public static void methodParamEndsWith(String name, String value, String endsWith) {
+        parameterEndsWith(name, value, endsWith);
+    }
+
+    public static void parameterNotNullAndNotEmpty(String name, String value) {
         parameterNotNull(name, value);
         if (value.equals("")) throw new MethodPreconditionFailedException("Parameter [" + name + "] is empty.");
     }
 
-    public static void methodParamNotNullAndNotEmpty(String name, char[] value) {
+    /**
+     * @deprecated use {@link #parameterNotNullAndNotEmpty(String, String)} instead.
+     * @param name
+     * @param value
+     */
+    @Deprecated
+    public static void methodParamNotNullAndNotEmpty(String name, String value) {
+        parameterNotNullAndNotEmpty(name, value);
+    }
+
+    public static void parameterNotNullAndNotEmpty(String name, char[] value) {
         if (value == null || value.length == 0) throw new MethodPreconditionFailedException("Parameter [" + name + "] is empty.");
     }
 
-    public static void methodParamInRange(String name, int value, int min, int max) {
+    /**
+     * @deprecated use {@link #parameterNotNullAndNotEmpty(String, char[])} instead.
+     * @param name
+     * @param value
+     */
+    @Deprecated
+    public static void methodParamNotNullAndNotEmpty(String name, char[] value) {
+        parameterNotNullAndNotEmpty(name, value);
+    }
+
+    public static void parameterInRange(String name, int value, int min, int max) {
         if (value < min || value > max) throw new MethodPreconditionFailedException("Parameter [" + name + "] out of bounds [" + min + " .. " + max + "]: " + value);
+    }
+
+    /**
+     * @deprecated use {@link #parameterInRange(String, int, int, int)} instead.
+     * @param name
+     * @param value
+     * @param min
+     * @param max
+     */
+    @Deprecated
+    public static void methodParamInRange(String name, int value, int min, int max) {
+        parameterInRange(name, value, min, max);
     }
 
 }
