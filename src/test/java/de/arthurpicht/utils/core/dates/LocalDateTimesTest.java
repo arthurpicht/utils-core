6b1e892d387c5cbaf770fa20a9da2e0c6ddb2745
Arthur Picht <picht@arthurpicht.de>
2025-02-20 10:29:43 +0100

    LocalDateTimesTest

--- src/test/java/de/arthurpicht/utils/core/dates/LocalDateTimesTest.java
+++ src/test/java/de/arthurpicht/utils/core/dates/LocalDateTimesTest.java
@@ -2,6 +2,7 @@ package de.arthurpicht.utils.core.dates;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -10,23 +11,225 @@ import static org.junit.jupiter.api.Assertions.*;
 class LocalDateTimesTest {
 
     @Test
-    void calculateBeginOfDay() {
-        LocalDate localDate = LocalDate.of(2025, 2, 20);
+    public void endOfMinusTwoWeeksFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
 
-        LocalDateTime beginOfToday = LocalDateTimes.calculateBeginOfDay(localDate, 0);
-        String beginOfTodayString = beginOfToday.toString();
-
-        assertEquals("2025-02-20T00:00", beginOfTodayString);
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, -2);
+        assertEquals("2024-05-19T23:59:59.999999999", endOfWeek.toString());
     }
 
     @Test
-    void calculateEndOfDay() {
-        LocalDate localDate = LocalDate.of(2025, 2, 20);
+    public void endOfMinusTwoWeeksFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 26);
+        assertEquals(DayOfWeek.SUNDAY, localDate.getDayOfWeek());
 
-        LocalDateTime endOfToday = LocalDateTimes.calculateEndOfDay(localDate, 0);
-        String endOfTodayString = endOfToday.toString();
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, -2);
+        assertEquals("2024-05-12T23:59:59.999999999", endOfWeek.toString());
+    }
 
-        assertEquals("2025-02-20T23:59:59.999999999", endOfTodayString);
+    @Test
+    public void endOfPreviousWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, -1);
+        assertEquals("2024-05-26T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfPreviousWeekFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 26);
+        assertEquals(DayOfWeek.SUNDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, -1);
+        assertEquals("2024-05-19T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfCurrentWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 0);
+        assertEquals("2024-06-02T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfCurrentWeekFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 26);
+        assertEquals(DayOfWeek.SUNDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 0);
+        assertEquals("2024-05-26T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfNextWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 1);
+        assertEquals("2024-06-09T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfNextWeekFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 26);
+        assertEquals(DayOfWeek.SUNDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 1);
+        assertEquals("2024-06-02T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfNextNextWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 2);
+        assertEquals("2024-06-16T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void endOfNextNextWeekFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 26);
+        assertEquals(DayOfWeek.SUNDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateEndOfWeek(localDate, 2);
+        assertEquals("2024-06-09T23:59:59.999999999", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfMinusTwoWeeksFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime beginOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, -2);
+        assertEquals("2024-05-13T00:00", beginOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfMinusTwoWeeksFromMonday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 27);
+        assertEquals(DayOfWeek.MONDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, -2);
+        assertEquals("2024-05-13T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfPreviousWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, -1);
+        assertEquals("2024-05-20T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfPreviousWeekFromMonday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        assertEquals(DayOfWeek.MONDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, -1);
+        assertEquals("2024-05-06T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfCurrentWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 0);
+        assertEquals("2024-05-27T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfCurrentWeekFromMonday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        assertEquals(DayOfWeek.MONDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 0);
+        assertEquals("2024-05-13T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfNextWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 1);
+        assertEquals("2024-06-03T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfNextWeekFromSunday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        assertEquals(DayOfWeek.MONDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 1);
+        assertEquals("2024-05-20T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfNextNextWeekFromTuesday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 28);
+        assertEquals(DayOfWeek.TUESDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 2);
+        assertEquals("2024-06-10T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfNextNextWeekFromMonday() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        assertEquals(DayOfWeek.MONDAY, localDate.getDayOfWeek());
+
+        LocalDateTime endOfWeek = LocalDateTimes.calculateBeginOfWeek(localDate, 2);
+        assertEquals("2024-05-27T00:00", endOfWeek.toString());
+    }
+
+    @Test
+    public void beginOfCurrentDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime beginOfDay = LocalDateTimes.calculateBeginOfDay(localDate, 0);
+        assertEquals("2024-05-13T00:00", beginOfDay.toString());
+    }
+
+    @Test
+    public void endOfCurrentDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime endOfDay = LocalDateTimes.calculateEndOfDay(localDate, 0);
+        assertEquals("2024-05-13T23:59:59.999999999", endOfDay.toString());
+    }
+
+    @Test
+    public void beginOfNextDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime beginOfDay = LocalDateTimes.calculateBeginOfDay(localDate, 1);
+        assertEquals("2024-05-14T00:00", beginOfDay.toString());
+    }
+
+    @Test
+    public void endOfNextDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime endOfDay = LocalDateTimes.calculateEndOfDay(localDate, 1);
+        assertEquals("2024-05-14T23:59:59.999999999", endOfDay.toString());
+    }
+
+    @Test
+    public void beginOfPreviousDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime beginOfDay = LocalDateTimes.calculateBeginOfDay(localDate, -1);
+        assertEquals("2024-05-12T00:00", beginOfDay.toString());
+    }
+
+    @Test
+    public void endOfPreviousDay() {
+        LocalDate localDate = LocalDate.of(2024, 5, 13);
+        LocalDateTime endOfDay = LocalDateTimes.calculateEndOfDay(localDate, -1);
+        assertEquals("2024-05-12T23:59:59.999999999", endOfDay.toString());
     }
 
 }
